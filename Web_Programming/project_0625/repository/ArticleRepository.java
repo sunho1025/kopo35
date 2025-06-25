eb79b1b2926b5a25e8ac4f811fcba777c5ec4aed
sunho1025 <sdu10722@gmail.com>
2025-06-25 16:34:08 +0900

    Delete Web_Programming/project_0625 directory

--- Web_Programming/project_0625/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.kopo_project.repository;
-
-import com.example.kopo_project.entity.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article,Long> {
-
-}
